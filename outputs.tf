e9d581ce1e72d62db78aa0873a1736a1bbf52a8b
Piotr Sierkin <piotr.sierkin@getindata.com>
2024-03-28 17:03:10 +0100

    chore: add database role name with fully qualified format

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,9 @@ output "name" {
   description = "Name of the database role"
   value       = one(snowflake_database_role.this[*].name)
 }
+
+output "name_fully_qualified" {
+  description = "Name of the database role in fully qualified format (\"DB_NAME\".\"ROLE_NAME\")"
+  value       = local.database_role_name
+}
+
