40391bf831ba473ffdc74ca14c48f0b6743fb2c7
Piotr Sierkin <piotr.sierkin@getindata.com>
2024-04-17 17:53:00 +0200

    chore: update test

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,16 +1,74 @@
+resource "snowflake_database" "this" {
+  name = "TEST_DB"
+}
+
+resource "snowflake_schema" "this" {
+  database = snowflake_database.this.name
+  name     = "BRONZE"
+}
+
+resource "snowflake_table" "table_1" {
+  database = snowflake_schema.this.database
+  schema   = snowflake_schema.this.name
+  name     = "TEST_TABLE_1"
+
+  column {
+    name     = "identity"
+    type     = "NUMBER(38,0)"
+    nullable = true
+
+    identity {
+      start_num = 1
+      step_num  = 3
+    }
+  }
+}
+
+resource "snowflake_table" "table_2" {
+  database = snowflake_schema.this.database
+  schema   = snowflake_schema.this.name
+  name     = "TEST_TABLE_2"
+
+  column {
+    name     = "identity"
+    type     = "NUMBER(38,0)"
+    nullable = true
+
+    identity {
+      start_num = 1
+      step_num  = 3
+    }
+  }
+}
+
+resource "snowflake_database_role" "db_role_1" {
+  database = snowflake_database.this.name
+  name     = "DB_ROLE_1"
+}
+
+resource "snowflake_database_role" "db_role_2" {
+  database = snowflake_database.this.name
+  name     = "DB_ROLE_2"
+}
+
+resource "snowflake_database_role" "db_role_3" {
+  database = snowflake_database.this.name
+  name     = "DB_ROLE_3"
+}
+
 module "snowflake_database_role" {
   source  = "../../"
   context = module.this.context
 
-  database_name = "TEST_PLAYGROUND"
-  comment       = "Database role for TEST_PLAYGROUND"
+  database_name = snowflake_database.this.name
+  comment       = "Database role for PS_PLAYGROUND"
   name          = "TEST_DB_ROLE"
 
 
-  parent_database_role = "TEST_DB_ROLE_1"
+  parent_database_role = snowflake_database_role.db_role_1.name
   granted_database_roles = [
-    "TEST_DB_ROLE_2",
-    "TEST_DB_ROLE_3"
+    snowflake_database_role.db_role_2.name,
+    snowflake_database_role.db_role_3.name
   ]
   database_grants = [
     {
@@ -20,16 +78,13 @@ module "snowflake_database_role" {
 
   schema_grants = [
     {
-      schema_name = "BRONZE"
+      schema_name = snowflake_schema.this.name
       privileges  = ["USAGE"]
     },
     {
       future_schemas_in_database = true
+      all_schemas_in_database    = true
       privileges                 = ["USAGE"]
-    },
-    {
-      all_schemas_in_database = true
-      privileges              = ["USAGE"]
     },
   ]
 
@@ -37,20 +92,13 @@ module "snowflake_database_role" {
     "TABLE" = [
       {
         privileges  = ["SELECT"]
-        object_name = "TEST_TABLE"
-        schema_name = "BRONZE"
+        object_name = snowflake_table.table_1.name
+        schema_name = snowflake_schema.this.name
       },
       {
         all_privileges = true
-        object_name    = "TEST_TABLE_2"
-        schema_name    = "BRONZE"
-      }
-    ]
-    "SECRET" = [
-      {
-        all_privileges = true
-        object_name    = "SERVICE_NOW_CREDS_PW"
-        schema_name    = "BRONZE"
+        object_name    = snowflake_table.table_2.name
+        schema_name    = snowflake_schema.this.name
       }
     ]
     "ALERT" = [
