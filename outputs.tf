2d84bde711dff8e85b469ec479af2d1777d718e8
Piotr Sierkin <piotr.sierkin@getindata.com>
2024-04-18 17:31:53 +0200

    chore: update output name

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@ output "name" {
   value       = one(snowflake_database_role.this[*].name)
 }
 
-output "name_fully_qualified" {
+output "fully_qualified_name" {
   description = "Name of the database role in fully qualified format (\"DB_NAME\".\"ROLE_NAME\")"
   value       = local.database_role_name
 }
